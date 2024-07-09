fab90a6fc64d469c6e76fd8eece7a242278028bb
ChandrasekarM <chandrasekar482003@gmail.com>
2024-07-09 15:50:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("smile").addEventListener("click",function(){document.getElementById("image").src="https://editors.dexerto.com/wp-content/uploads/2024/06/16/boys-homelander.jpg"});
+document.getElementById("angry").addEventListener("click",function(){document.getElementById("image").src="https://static1.srcdn.com/wordpress/wp-content/uploads/2022/06/He-Lost-But-The-Boys-Herogasm-Fight-Made-Homelander-Even-Scarier.jpg"});
+document.getElementById("fight").addEventListener("click",function(){document.getElementById("image").src="https://variety.com/wp-content/uploads/2024/06/The-Boys-Homelander.png"});
+document.getElementById("normal").addEventListener("click",function(){document.getElementById("image").src="https://imgix.ranker.com/user_node_img/50115/1002285589/original/1002285589-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375"});
